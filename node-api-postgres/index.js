d237339852bcc60dea48beea8a737a839da6ed74
SehajbirBrar <120230498+SehajbirBrar@users.noreply.github.com>
2024-04-23 14:18:37 -0600

    Updating project in progress

---
+++ node-api-postgres/index.js
@@ -0,0 +1,43 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
+})
+
+
+
+  
